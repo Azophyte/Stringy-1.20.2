3cb8e1cd3175f3f337409ffdb91df24bee3f9027
Azo <Azophyte@gmail.com>
2023-10-19 15:39:35 +0100

    Initial commit

---
+++ src/main/java/net/azo/stringy/StringyClient.java
@@ -0,0 +1,10 @@
+package net.azo.stringy;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StringyClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
